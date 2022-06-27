bb98dbd60907fe3eb75950445a08732a72d9f0de
Eslam El-Meniawy <eslam.elmeniawy@gmail.com>
2022-06-27 13:23:05 +0200

    Enable logging

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import {View} from 'react-native';
 import {Provider as ReduxProvider} from 'react-redux';
 import {Provider as PaperProvider} from 'react-native-paper';
 import {ScaledSheet} from 'react-native-size-matters';
+import {configureLog} from 'roqay-react-native-common-components';
+import Config from 'react-native-config';
 
 import {store} from './store';
 import AppColors from './enums/AppColors';
@@ -11,7 +13,24 @@ import {setI18nConfig} from './core';
 
 import NavigationContainer from './navigation/NavigationContainer';
 
+const getLogMessage = (message: string) => {
+  return `## App: ${message}`;
+};
+
 export default () => {
+  // Log initialization.
+  React.useEffect(() => {
+    configureLog({
+      // TODO: Replace with app name.
+      appName: 'TempApp',
+      firebaseLogLevels:
+        Config.ENABLE_FIREBASE_LOG === 'true'
+          ? ['LOG', 'WARN', 'ERROR']
+          : undefined,
+      isLocalLogEnable: Config.ENABLE_LOCAL_LOG === 'true',
+    });
+  }, []);
+
   // Localization initialization.
   React.useEffect(() => {
     setI18nConfig();
