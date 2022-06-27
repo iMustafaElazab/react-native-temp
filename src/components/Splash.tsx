3322fc1517361990f5b339a8a1cc1ba833555d0b
Eslam El-Meniawy <eslam.elmeniawy@gmail.com>
2022-06-27 13:11:06 +0200

    Display environment name in splash

--- src/components/Splash.tsx
+++ src/components/Splash.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import {StatusBar, View} from 'react-native';
 import {ScaledSheet} from 'react-native-size-matters';
 import {Text} from 'roqay-react-native-common-components';
+import Config from 'react-native-config';
 
 export default () => {
   // TODO: Design splash.
   return (
-    <Text type="bold" style={styles.text}>
-      Splash Screen
-    </Text>
+    <>
+      <Text type="bold" style={styles.text}>
+        Splash Screen
+      </Text>
+      <Text style={styles.text}>{`Environment: ${Config.ENV_NAME}`}</Text>
+    </>
   );
 };
 
