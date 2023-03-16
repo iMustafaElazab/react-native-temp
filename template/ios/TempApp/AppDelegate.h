42b26b9421562526be8e3fbd725dc6447103a9cf
Eslam El-Meniawy <eslam.elmeniawy@gmail.com>
2023-03-16 13:54:57 +0200

    feat: upgrade `ReactNative` & dependencies

--- template/ios/TempApp/AppDelegate.h
+++ template/ios/TempApp/AppDelegate.h
@@ -1,11 +1,9 @@
-#import <React/RCTBridgeDelegate.h>
+#import <RCTAppDelegate.h>
 #import <UIKit/UIKit.h>
 
 // Imports for notifications.
 #import <UserNotifications/UNUserNotificationCenter.h>
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate, UNUserNotificationCenterDelegate>
-
-@property (nonatomic, strong) UIWindow *window;
+@interface AppDelegate : RCTAppDelegate <UNUserNotificationCenterDelegate>
 
 @end
