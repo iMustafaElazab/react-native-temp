907d24a326c47815327b7ce169d37d0ef7c308c9
Eslam El Meniawy <eelmeniawy@ejada.com>
2023-09-23 18:41:15 +0300

    build: add build related packages

--- template/babel.config.js
+++ template/babel.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  env: {
+    production: {
+      plugins: ['react-native-paper/babel'],
+    },
+  },
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.tsx', '.ts', '.js', '.json'],
+      },
+    ],
+  ],
 };
