4d8dcca4ecbb2f12c548fe37d02f5c8e8437f3d1
Eslam El Meniawy <eelmeniawy@ejada.com>
2023-10-12 12:16:07 +0300

    feat: add `naming-convention` rules

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -1,4 +1,65 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  ignorePatterns: ['.eslintrc.js'],
+  parser: '@typescript-eslint/parser',
+  rules: {
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'default',
+        format: ['camelCase'],
+      },
+      {
+        selector: 'variable',
+        format: ['camelCase', 'UPPER_CASE'],
+      },
+      {
+        selector: 'variable',
+        types: ['function'],
+        format: ['PascalCase', 'camelCase'],
+      },
+      {
+        selector: 'function',
+        format: ['PascalCase', 'camelCase'],
+      },
+      {
+        selector: 'parameter',
+        format: ['camelCase'],
+        leadingUnderscore: 'allow',
+      },
+      {
+        selector: 'enumMember',
+        format: ['UPPER_CASE'],
+      },
+      {
+        selector: 'memberLike',
+        modifiers: ['private'],
+        format: ['camelCase'],
+        leadingUnderscore: 'require',
+      },
+      {
+        selector: 'typeLike',
+        format: ['PascalCase'],
+      },
+      {
+        selector: [
+          'classProperty',
+          'objectLiteralProperty',
+          'typeProperty',
+          'classMethod',
+          'objectLiteralMethod',
+          'typeMethod',
+          'accessor',
+          'enumMember',
+        ],
+        format: null,
+        modifiers: ['requiresQuotes'],
+      },
+    ],
+  },
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    project: './tsconfig.json',
+  },
 };
