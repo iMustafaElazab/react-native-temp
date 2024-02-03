4bbe35baeb689010f6d2d018b800a3e7af562085
Eslam El Meniawy <eelmeniawy@ejada.com>
2024-02-03 15:04:06 +0200

    build: change `reactotron` version

--- template/src/App/useLogInitialization.ts
+++ template/src/App/useLogInitialization.ts
@@ -2,10 +2,16 @@ import {configureLog} from '@eslam-elmeniawy/react-native-common-components';
 import * as React from 'react';
 import {default as Config} from 'react-native-config';
 import {getApplicationName} from 'react-native-device-info';
+import {QueryClientManager, reactotronReactQuery} from 'reactotron-react-query';
+import {queryClient} from '@src/utils';
 
 export const useLogInitialization = () => {
   React.useEffect(() => {
     const appName = getApplicationName();
+
+    const queryClientManager = new QueryClientManager({
+      queryClient,
+    });
 
     configureLog?.({
       appName: appName,
@@ -14,6 +20,12 @@ export const useLogInitialization = () => {
           ? ['LOG', 'WARN', 'ERROR']
           : undefined,
       isLocalLogEnable: Config.ENABLE_LOCAL_LOG === 'true',
+      pluginCreators: [reactotronReactQuery(queryClientManager)],
+      clientOptions: {
+        onDisconnect: () => {
+          queryClientManager.unsubscribe();
+        },
+      },
     });
   }, []);
 };
