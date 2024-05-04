3ccc4a8342d208dd7b03b1af0289a74e13039842
Eslam El-Meniawy <eelmeniawy@ejada.com>
2024-05-04 17:00:24 +0300

    feat: handle protected routes

--- template/src/navigation/RootStack.tsx
+++ template/src/navigation/RootStack.tsx
@@ -1,38 +1,45 @@
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import * as React from 'react';
 import type {RootStackParamList} from '@src/navigation';
-
-// Screens.
 import {Splash, Login, Home, Notifications} from '@src/screens';
-
-// Navigators.
-// TODO: Add navigators imports here.
-
-// Modals.
-// TODO: Add modals imports here.
+import {useAppSelector} from '@src/store';
 
 const stack = createNativeStackNavigator<RootStackParamList>();
 
-export default React.memo(() => (
-  <stack.Navigator
-    id="RootStack"
-    initialRouteName="splash"
-    screenOptions={{headerShown: false}}>
-    {/* Screens */}
-    <stack.Screen name="splash" component={Splash} />
-    <stack.Screen name="login" component={Login} />
-    <stack.Screen name="home" component={Home} />
-    <stack.Screen name="notifications" component={Notifications} />
+export default React.memo(() => {
+  // #region Redux
+  const {user} = useAppSelector(state => state.user);
+  // #endregion
 
-    {/* Navigators */}
-    {/* TODO: Add nested navigators here. */}
+  return (
+    <stack.Navigator
+      id="RootStack"
+      initialRouteName="splash"
+      screenOptions={{headerShown: false}}>
+      {/* Screens */}
+      <stack.Screen name="splash" component={Splash} />
+      {!user && (
+        <stack.Group>
+          <stack.Screen name="login" component={Login} />
+        </stack.Group>
+      )}
+      {user && (
+        <stack.Group>
+          <stack.Screen name="home" component={Home} />
+          <stack.Screen name="notifications" component={Notifications} />
+        </stack.Group>
+      )}
 
-    {/* Modals */}
-    <stack.Group
-      screenOptions={{
-        presentation: 'transparentModal',
-      }}>
-      <>{/* TODO: Add modals screens here. */}</>
-    </stack.Group>
-  </stack.Navigator>
-));
+      {/* Navigators */}
+      {/* TODO: Add nested navigators here. */}
+
+      {/* Modals */}
+      <stack.Group
+        screenOptions={{
+          presentation: 'transparentModal',
+        }}>
+        <>{/* TODO: Add modals screens here. */}</>
+      </stack.Group>
+    </stack.Navigator>
+  );
+});
