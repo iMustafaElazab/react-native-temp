b59057e120231283c709f8da2c2e8c605f3b6ab9
Abdelrahman Yasser <abdelrahman.yasser.365@gmail.com>
2024-05-29 12:46:30 +0300

    test: add , ,  and  tests

--- template/src/utils/RandomUtils.ts
+++ template/src/utils/RandomUtils.ts
@@ -4,6 +4,22 @@
  * @param {number} min - The minimum value for the random integer.
  * @param {number} max - The maximum value for the random integer.
  * @returns {number} A random integer between min and max (inclusive).
+ * @throws {Error} If the minimum value is negative, maximum value is negative, or minimum value is greater than the maximum value.
  */
-export const randomIntFromInterval = (min: number, max: number): number =>
-  Math.floor(Math.random() * (max - min + 1) + min);
+export const randomIntFromInterval = (min: number, max: number): number => {
+  if (min < 0) {
+    throw new Error('Minimum value must be positive numbers.');
+  }
+
+  if (max < 0) {
+    throw new Error('Maximum value must be positive numbers.');
+  }
+
+  if (min > max) {
+    throw new Error(
+      'Minimum value must be less than or equal to the maximum value.',
+    );
+  }
+
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
--- template/src/utils/ResponsiveDimension.ts
+++ template/src/utils/ResponsiveDimension.ts
@@ -1,5 +1,50 @@
-export const percentWidth = (fullWidth: number, percent?: number) =>
-  fullWidth * ((percent ?? 100) / 100);
+/**
+ * Calculates the width based on the percentage of the full width.
+ *
+ * @param fullWidth The full width value.
+ * @param percent The percentage value (optional). Must be between 0 and 100.
+ * @returns The calculated width based on the percentage provided. If no percentage is provided, defaults to 100%.
+ * @throws {Error} When fullWidth is a negative number.
+ * @throws {Error} When percent is a negative number.
+ * @throws {Error} When percent is greater than 100.
+ */ export const percentWidth = (fullWidth: number, percent?: number) => {
+  if (fullWidth < 0) {
+    throw new Error('fullWidth must be positive numbers.');
+  }
 
-export const percentHeight = (fullHeight: number, percent?: number) =>
-  fullHeight * ((percent ?? 100) / 100);
+  if (percent !== undefined && percent < 0) {
+    throw new Error('percent must be positive numbers.');
+  }
+
+  if (percent !== undefined && percent > 100) {
+    throw new Error('percent must be less than or equal to 100.');
+  }
+
+  return fullWidth * ((percent ?? 100) / 100);
+};
+
+/**
+ * Calculates the height based on the percentage of the full height.
+ *
+ * @param fullHeight The full height value.
+ * @param percent The percentage value (optional). Must be between 0 and 100.
+ * @returns The calculated height based on the percentage provided. If no percentage is provided, defaults to 100%.
+ * @throws {Error} When fullHeight is a negative number.
+ * @throws {Error} When percent is a negative number.
+ * @throws {Error} When percent is greater than 100.
+ */
+export const percentHeight = (fullHeight: number, percent?: number) => {
+  if (fullHeight < 0) {
+    throw new Error('fullHeight must be positive numbers.');
+  }
+
+  if (percent !== undefined && percent < 0) {
+    throw new Error('percent must be positive numbers.');
+  }
+
+  if (percent !== undefined && percent > 100) {
+    throw new Error('percent must be less than or equal to 100.');
+  }
+
+  return fullHeight * ((percent ?? 100) / 100);
+};
