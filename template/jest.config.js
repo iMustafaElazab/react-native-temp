1d3b0b144aef6dd4e14dcc6a7badc1d982b23219
Eslam El-Meniawy <eelmeniawy@ejada.com>
2024-05-31 22:48:47 +0300

    test: update test settings

--- template/jest.config.js
+++ template/jest.config.js
@@ -14,20 +14,9 @@ module.exports = {
   setupFiles: [
     '<rootDir>/node_modules/react-native-gesture-handler/jestSetup.js',
   ],
-  // transformIgnorePatterns: [
-  //   'node_modules/(?!(jest-)?(@react-native|react-native|@react-native-community|react-native-size-matters|react-redux|@react-navigation|reactotron-react-native-mmkv)/)',
-  // ],
   transformIgnorePatterns: [
     'node_modules/(?!(jest-)?(react-native|@react-native|@react-native-community|@react-navigation|react-redux|react-native-size-matters|reactotron-react-native-mmkv)/)',
   ],
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.{ts,tsx}'],
-  coveragePathIgnorePatterns: [
-    'src/assets',
-    'src/enums',
-    'src/translations',
-    'src/core/Api/entities',
-    'src/core/Api/fakers',
-    'src/core/Api/responses',
-  ],
 };
