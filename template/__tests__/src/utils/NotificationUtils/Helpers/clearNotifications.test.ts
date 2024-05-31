7c59019d1ac0b636fbbbb6157ec7e9aa29bd03e1
Eslam El-Meniawy <eelmeniawy@ejada.com>
2024-05-31 22:41:44 +0300

    test: add more tests

--- template/__tests__/src/utils/NotificationUtils/Helpers/clearNotifications.test.ts
+++ template/__tests__/src/utils/NotificationUtils/Helpers/clearNotifications.test.ts
@@ -1,84 +1,81 @@
-import {describe, test, expect, jest, afterEach} from '@jest/globals';
+import {test, expect, jest, afterEach} from '@jest/globals';
 import {default as PushNotificationIOS} from '@react-native-community/push-notification-ios';
 import {Platform} from 'react-native';
 import {default as PushNotification} from 'react-native-push-notification';
 import {clearNotifications} from '@src/utils/NotificationUtils/Helpers';
 
-describe('clearNotifications HAPPY PATH', () => {
-  const mockCancelLocalNotification = jest.spyOn(
-    PushNotification,
-    'cancelLocalNotification',
-  );
+const mockCancelLocalNotification = jest.spyOn(
+  PushNotification,
+  'cancelLocalNotification',
+);
 
-  const mockRemoveDeliveredNotifications = jest.spyOn(
-    PushNotificationIOS,
-    'removeDeliveredNotifications',
-  );
+const mockRemoveDeliveredNotifications = jest.spyOn(
+  PushNotificationIOS,
+  'removeDeliveredNotifications',
+);
 
-  afterEach(() => {
-    mockCancelLocalNotification?.mockClear();
-    mockRemoveDeliveredNotifications?.mockClear();
-  });
-
-  test('should clear local notification when notification ID is a string', () => {
-    const notification = {
-      id: '123',
-      key: '123',
-      title: 'Test',
-      message: 'Test message',
-    };
-
-    clearNotifications(notification);
-    expect(mockCancelLocalNotification).toHaveBeenCalledWith('123');
-
-    if (Platform.OS === 'ios') {
-      expect(mockRemoveDeliveredNotifications).toHaveBeenCalledWith(['123']);
-    }
-  });
+afterEach(() => {
+  mockCancelLocalNotification?.mockClear();
+  mockRemoveDeliveredNotifications?.mockClear();
 });
 
-describe('clearNotifications EDGE CASES', () => {
-  const mockCancelLocalNotification = jest.spyOn(
-    PushNotification,
-    'cancelLocalNotification',
-  );
+test('should clear local notification when notification ID is a string', () => {
+  const notification = {
+    id: '123',
+    key: '123',
+    title: 'Test',
+    message: 'Test message',
+  };
 
-  const mockRemoveDeliveredNotifications = jest.spyOn(
-    PushNotificationIOS,
-    'removeDeliveredNotifications',
-  );
+  clearNotifications(notification);
+  expect(mockCancelLocalNotification).toHaveBeenCalledWith('123');
 
-  afterEach(() => {
-    mockCancelLocalNotification?.mockClear();
-    mockRemoveDeliveredNotifications?.mockClear();
-  });
+  if (Platform.OS === 'ios') {
+    expect(mockRemoveDeliveredNotifications).toHaveBeenCalledWith(['123']);
+  }
+});
 
-  test('should not clear local notification when notification ID is undefined or null', () => {
-    const notification = {
-      id: undefined,
-      key: '',
-      title: 'Test',
-      message: 'Test message',
-    };
+test('should not clear local notification when notification ID is undefined or null', () => {
+  const notification = {
+    id: undefined,
+    key: '',
+    title: 'Test',
+    message: 'Test message',
+  };
 
-    clearNotifications(notification);
-    expect(mockCancelLocalNotification).not.toHaveBeenCalled();
+  clearNotifications(notification);
+  expect(mockCancelLocalNotification).not.toHaveBeenCalled();
 
-    if (Platform.OS === 'ios') {
-      expect(mockRemoveDeliveredNotifications).not.toHaveBeenCalled();
-    }
-  });
-
-  test('should not clear notification when ID is not a string', () => {
-    const notification = {
-      id: 123,
-      key: '123',
-      title: 'Test',
-      message: 'Test message',
-    };
-
-    clearNotifications(notification);
-    expect(mockCancelLocalNotification).not.toHaveBeenCalled();
+  if (Platform.OS === 'ios') {
     expect(mockRemoveDeliveredNotifications).not.toHaveBeenCalled();
-  });
+  }
+});
+
+test('should not clear notification when ID is not a string', () => {
+  const notification = {
+    id: 123,
+    key: '123',
+    title: 'Test',
+    message: 'Test message',
+  };
+
+  clearNotifications(notification);
+  expect(mockCancelLocalNotification).not.toHaveBeenCalled();
+  expect(mockRemoveDeliveredNotifications).not.toHaveBeenCalled();
+});
+
+test('should not clear iOS notification when platform is not iOS', () => {
+  const notification = {
+    id: '123',
+    key: '123',
+    title: 'Test',
+    message: 'Test message',
+  };
+
+  clearNotifications(notification);
+  expect(mockCancelLocalNotification).toHaveBeenCalledWith('123');
+
+  if (Platform.OS !== 'ios') {
+    expect(mockRemoveDeliveredNotifications).not.toHaveBeenCalled();
+  }
 });
--- template/__tests__/src/utils/NotificationUtils/Helpers/processUserNotification.test.ts
+++ template/__tests__/src/utils/NotificationUtils/Helpers/processUserNotification.test.ts
@@ -1,32 +1,66 @@
-import {describe, test, expect, jest} from '@jest/globals';
+import {test, expect, jest} from '@jest/globals';
 
 import {processUserNotification} from '@src/utils/NotificationUtils/Helpers';
 
-describe('processUserNotification', () => {
-  test('should update unread notifications count in user state when called with valid notification', () => {
-    const notification = {
-      id: 1,
-      key: '1',
-      title: 'Test',
-      message: 'Test message',
-    };
+const notification = {
+  id: 1,
+  key: '1',
+  title: 'Test',
+  message: 'Test message',
+};
 
-    const stateUser = {id: 1, unreadNotificationsCount: 0};
-    const newNotificationsCount = 5;
-    const shouldSkipOpenNotificationsScreen = false;
+const stateUser = {id: 1, unreadNotificationsCount: 0};
 
-    jest.spyOn(console, 'info').mockImplementation(() => {});
+test('should update unread notifications count in user state when called with valid notification', () => {
+  const newNotificationsCount = 5;
+  const shouldSkipOpenNotificationsScreen = false;
+  jest.spyOn(console, 'info').mockImplementation(() => {});
 
-    processUserNotification(
-      notification,
-      stateUser,
-      newNotificationsCount,
-      shouldSkipOpenNotificationsScreen,
-    );
+  processUserNotification(
+    notification,
+    stateUser,
+    newNotificationsCount,
+    shouldSkipOpenNotificationsScreen,
+  );
 
-    expect(console.info).toHaveBeenCalledWith(
-      '## NotificationUtils::Helpers:: userWithNewNotificationsCount',
-      {...stateUser, unreadNotificationsCount: newNotificationsCount},
-    );
-  });
+  expect(console.info).toHaveBeenCalledWith(
+    '## NotificationUtils::Helpers:: userWithNewNotificationsCount',
+    {...stateUser, unreadNotificationsCount: newNotificationsCount},
+  );
+});
+
+test('should update unread notifications count to zero in user state when called with zero newNotificationsCount', () => {
+  const newNotificationsCount = 0;
+  const shouldSkipOpenNotificationsScreen = false;
+  jest.spyOn(console, 'info').mockImplementation(() => {});
+
+  processUserNotification(
+    notification,
+    stateUser,
+    newNotificationsCount,
+    shouldSkipOpenNotificationsScreen,
+  );
+
+  expect(console.info).toHaveBeenCalledWith(
+    '## NotificationUtils::Helpers:: userWithNewNotificationsCount',
+    {...stateUser, unreadNotificationsCount: newNotificationsCount},
+  );
+});
+
+test('should update unread notifications count to zero in user state when called with negative newNotificationsCount', () => {
+  const newNotificationsCount = -1;
+  const shouldSkipOpenNotificationsScreen = false;
+  jest.spyOn(console, 'info').mockImplementation(() => {});
+
+  processUserNotification(
+    notification,
+    stateUser,
+    newNotificationsCount,
+    shouldSkipOpenNotificationsScreen,
+  );
+
+  expect(console.info).toHaveBeenCalledWith(
+    '## NotificationUtils::Helpers:: userWithNewNotificationsCount',
+    {...stateUser, unreadNotificationsCount: newNotificationsCount},
+  );
 });
